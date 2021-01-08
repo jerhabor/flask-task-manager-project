cd0612c8afe3388f56b3fb5ce7827b71deeb02d3
Jesse Erhabor <jess.erhabor@gmail.com>
2021-01-08 10:26:24 +0000

    Add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 // Initialisation for Mobile Collapse function from Materialize
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
   });
